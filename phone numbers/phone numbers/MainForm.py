6a05620410e5a674901645c1f8cdce71bf25cb72
someoneonthe <mullen.n@stu.janesville.k12.wi.us>
2024-09-26 09:17:06 -0500

    text box test

--- phone numbers/phone numbers/MainForm.py
+++ phone numbers/phone numbers/MainForm.py
@@ -9,101 +9,69 @@ class MainForm(Form):
         self.InitializeComponent()
     
     def InitializeComponent(self):
-        self._label1 = System.Windows.Forms.Label()
         self._button1 = System.Windows.Forms.Button()
         self._button2 = System.Windows.Forms.Button()
         self._button3 = System.Windows.Forms.Button()
-        self._button4 = System.Windows.Forms.Button()
-        self._button5 = System.Windows.Forms.Button()
-        self._button6 = System.Windows.Forms.Button()
-        self._button7 = System.Windows.Forms.Button()
+        self._textBox1 = System.Windows.Forms.TextBox()
         self.SuspendLayout()
-        # 
-        # label1
-        # 
-        self._label1.Location = System.Drawing.Point(13, 13)
-        self._label1.Name = "label1"
-        self._label1.Size = System.Drawing.Size(873, 253)
-        self._label1.TabIndex = 0
-        self._label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         # 
         # button1
         # 
-        self._button1.Location = System.Drawing.Point(12, 269)
+        self._button1.Location = System.Drawing.Point(13, 270)
         self._button1.Name = "button1"
-        self._button1.Size = System.Drawing.Size(180, 69)
+        self._button1.Size = System.Drawing.Size(147, 68)
         self._button1.TabIndex = 1
-        self._button1.Text = "Noodles & company"
+        self._button1.Text = "show"
         self._button1.UseVisualStyleBackColor = True
+        self._button1.Click += self.Button1Click
         # 
         # button2
         # 
-        self._button2.Location = System.Drawing.Point(254, 269)
+        self._button2.Location = System.Drawing.Point(397, 270)
         self._button2.Name = "button2"
-        self._button2.Size = System.Drawing.Size(180, 69)
+        self._button2.Size = System.Drawing.Size(145, 68)
         self._button2.TabIndex = 2
-        self._button2.Text = "Target"
+        self._button2.Text = "clear"
         self._button2.UseVisualStyleBackColor = True
+        self._button2.Click += self.Button2Click
         # 
         # button3
         # 
-        self._button3.Location = System.Drawing.Point(484, 269)
+        self._button3.Location = System.Drawing.Point(750, 269)
         self._button3.Name = "button3"
-        self._button3.Size = System.Drawing.Size(180, 69)
+        self._button3.Size = System.Drawing.Size(147, 68)
         self._button3.TabIndex = 3
-        self._button3.Text = "button3"
+        self._button3.Text = "Exit"
         self._button3.UseVisualStyleBackColor = True
+        self._button3.Click += self.Button3Click
         # 
-        # button4
+        # textBox1
         # 
-        self._button4.Location = System.Drawing.Point(706, 269)
-        self._button4.Name = "button4"
-        self._button4.Size = System.Drawing.Size(180, 69)
-        self._button4.TabIndex = 4
-        self._button4.Text = "button4"
-        self._button4.UseVisualStyleBackColor = True
-        # 
-        # button5
-        # 
-        self._button5.Location = System.Drawing.Point(137, 344)
-        self._button5.Name = "button5"
-        self._button5.Size = System.Drawing.Size(180, 69)
-        self._button5.TabIndex = 5
-        self._button5.Text = "button5"
-        self._button5.UseVisualStyleBackColor = True
-        # 
-        # button6
-        # 
-        self._button6.Location = System.Drawing.Point(368, 344)
-        self._button6.Name = "button6"
-        self._button6.Size = System.Drawing.Size(180, 69)
-        self._button6.TabIndex = 6
-        self._button6.Text = "button6"
-        self._button6.UseVisualStyleBackColor = True
-        # 
-        # button7
-        # 
-        self._button7.Location = System.Drawing.Point(598, 344)
-        self._button7.Name = "button7"
-        self._button7.Size = System.Drawing.Size(180, 69)
-        self._button7.TabIndex = 7
-        self._button7.Text = "button7"
-        self._button7.UseVisualStyleBackColor = True
+        self._textBox1.Location = System.Drawing.Point(13, 13)
+        self._textBox1.Name = "textBox1"
+        self._textBox1.Size = System.Drawing.Size(873, 31)
+        self._textBox1.TabIndex = 4
         # 
         # MainForm
         # 
-        self.BackColor = System.Drawing.Color.Black
+        self.BackColor = System.Drawing.SystemColors.ActiveBorder
         self.ClientSize = System.Drawing.Size(909, 446)
-        self.Controls.Add(self._button7)
-        self.Controls.Add(self._button6)
-        self.Controls.Add(self._button5)
-        self.Controls.Add(self._button4)
+        self.Controls.Add(self._textBox1)
         self.Controls.Add(self._button3)
         self.Controls.Add(self._button2)
         self.Controls.Add(self._button1)
-        self.Controls.Add(self._label1)
         self.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
         self.Name = "MainForm"
         self.Text = "phone numbers"
         self.ResumeLayout(False)
+        self.PerformLayout()
 
+
+    def Button1Click(self, sender, e):
+        self._textBox1.Text = "Noodles & company - 608 756 9700 Target - 608 754 8331"
+        
+    def Button2Click(self, sender, e):
+        self._textBox1.Text = ""
+
+    def Button3Click(self, sender, e):
+        Application.Exit()
