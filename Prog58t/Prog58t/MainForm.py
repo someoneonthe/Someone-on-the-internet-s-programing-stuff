ebbfac11a51e95a444c828dceb9d33ba86423b35
someoneonthe <mullen.n@stu.janesville.k12.wi.us>
2024-09-27 09:30:25 -0500

    Add files via upload

---
+++ Prog58t/Prog58t/MainForm.py
@@ -0,0 +1,20 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+    def __init__(self):
+        self.InitializeComponent()
+    
+    def InitializeComponent(self):
+        self.SuspendLayout()
+        # 
+        # MainForm
+        # 
+        self.ClientSize = System.Drawing.Size(690, 462)
+        self.Name = "MainForm"
+        self.Text = "Prog58t"
+        self.ResumeLayout(False)
+
