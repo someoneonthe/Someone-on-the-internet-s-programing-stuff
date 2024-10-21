2395db8efb3ca29fb0e7da1481ccb33d92bb78d4
someoneonthe <mullen.n@stu.janesville.k12.wi.us>
2024-10-21 09:10:26 -0500

    10/21/2024

---
+++ prog 162 b/prog 162 b/MainForm.py
@@ -0,0 +1,99 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+    def __init__(self):
+        self.InitializeComponent()
+    
+    def InitializeComponent(self):
+        self._listBox1 = System.Windows.Forms.ListBox()
+        self._button1 = System.Windows.Forms.Button()
+        self._button2 = System.Windows.Forms.Button()
+        self._button3 = System.Windows.Forms.Button()
+        self._button4 = System.Windows.Forms.Button()
+        self.SuspendLayout()
+        # 
+        # listBox1
+        # 
+        self._listBox1.FormattingEnabled = True
+        self._listBox1.ItemHeight = 25
+        self._listBox1.Location = System.Drawing.Point(12, 12)
+        self._listBox1.Name = "listBox1"
+        self._listBox1.Size = System.Drawing.Size(508, 304)
+        self._listBox1.TabIndex = 0
+        # 
+        # button1
+        # 
+        self._button1.Location = System.Drawing.Point(13, 335)
+        self._button1.Name = "button1"
+        self._button1.Size = System.Drawing.Size(110, 103)
+        self._button1.TabIndex = 1
+        self._button1.Text = "calculate"
+        self._button1.UseVisualStyleBackColor = True
+        self._button1.Click += self.Button1Click
+        # 
+        # button2
+        # 
+        self._button2.Location = System.Drawing.Point(182, 335)
+        self._button2.Name = "button2"
+        self._button2.Size = System.Drawing.Size(107, 103)
+        self._button2.TabIndex = 2
+        self._button2.Text = "clear"
+        self._button2.UseVisualStyleBackColor = True
+        self._button2.Click += self.Button2Click
+        # 
+        # button3
+        # 
+        self._button3.Location = System.Drawing.Point(365, 335)
+        self._button3.Name = "button3"
+        self._button3.Size = System.Drawing.Size(115, 103)
+        self._button3.TabIndex = 3
+        self._button3.Text = "quit"
+        self._button3.UseVisualStyleBackColor = True
+        self._button3.Click += self.Button3Click
+        # 
+        # button4
+        # 
+        self._button4.Location = System.Drawing.Point(510, 428)
+        self._button4.Name = "button4"
+        self._button4.Size = System.Drawing.Size(10, 10)
+        self._button4.TabIndex = 4
+        self._button4.Text = "button4"
+        self._button4.UseVisualStyleBackColor = True
+        self._button4.Click += self.Button4Click
+        # 
+        # MainForm
+        # 
+        self.ClientSize = System.Drawing.Size(532, 450)
+        self.Controls.Add(self._button4)
+        self.Controls.Add(self._button3)
+        self.Controls.Add(self._button2)
+        self.Controls.Add(self._button1)
+        self.Controls.Add(self._listBox1)
+        self.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self.Name = "MainForm"
+        self.Text = "prog 162 b"
+        self.ResumeLayout(False)
+
+
+    def Button1Click(self, sender, e):
+        heading = "year\t\tPopulation (in mil.)"
+        self._listBox1.Items.Add(heading)
+        pop = 243
+        for year in range(1990,2016):
+            line = str(year) + "\t\t" + str(int(pop))
+            self._listBox1.Items.Add(line)
+            pop *= 1.029
+    def Button2Click(self, sender, e):
+        self._listBox1.Items.Clear()
+
+    def Button3Click(self, sender, e):
+        Application.Exit()
+
+    def Button4Click(self, sender, e):
+        self._button1.Text = "stinky"
+        self._button2.Text = "poopy"
+        self._button3.Text = "baby"
